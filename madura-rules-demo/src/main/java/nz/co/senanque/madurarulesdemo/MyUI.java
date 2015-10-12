9c8fca0739fcbe111107f828d46d3efd64e3dfc8
rogerparkinson <roger@senanque.co.nz>
2015-10-13 11:23:24 +1300

    sample still works with adjusted pom.xml

--- madura-rules-demo/src/main/java/nz/co/senanque/madurarulesdemo/MyUI.java
+++ madura-rules-demo/src/main/java/nz/co/senanque/madurarulesdemo/MyUI.java
@@ -1,25 +1,97 @@
 package nz.co.senanque.madurarulesdemo;
 
+import javax.servlet.annotation.WebListener;
 import javax.servlet.annotation.WebServlet;
 
+import nz.co.senanque.vaadin.Hints;
+import nz.co.senanque.vaadin.HintsImpl;
+import nz.co.senanque.vaadin.application.MaduraSessionManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.web.context.ContextLoaderListener;
+
+import ch.qos.logback.ext.spring.web.LogbackConfigListener;
+
 import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.external.org.slf4j.Logger;
+import com.vaadin.external.org.slf4j.LoggerFactory;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.spring.annotation.EnableVaadin;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.annotation.UIScope;
+import com.vaadin.spring.navigator.SpringViewProvider;
+import com.vaadin.spring.server.SpringVaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-/**
- *
- */
 @Theme("mytheme")
+@Title("Madura Rules Demo")
 @Widgetset("nz.co.senanque.madurarulesdemo.MyAppWidgetset")
+@SpringUI
 public class MyUI extends UI {
 
+	private static Logger m_logger = LoggerFactory.getLogger(MyUI.class);
+	
+	@Autowired private SpringViewProvider viewProvider;
+	@Autowired private MaduraSessionManager m_maduraSessionManager;
+
+    @WebServlet(name = "MyUIServlet", urlPatterns = "/*", asyncSupported = true)
+    public static class MyUIServlet extends SpringVaadinServlet {
+
+		private static final long serialVersionUID = 1L;
+    }
+
+    @WebListener
+    public static class MyContextLoaderListener extends ContextLoaderListener {
+    	// This causes the applicationContext.xml context file to be loaded
+    	// per session.
+    }
+
+    @WebListener
+    public static class MyLogbackConfigListener extends LogbackConfigListener {
+    	// Need this to init logback correctly
+    }
+    
+    @Configuration
+    @EnableVaadin
+    @ComponentScan(basePackages = {
+    		"nz.co.senanque.vaadin",
+    		"nz.co.senanque.validationengine"})
+    @PropertySource("classpath:config.properties")
+    public static class MyConfiguration {
+    	
+    	public MyConfiguration() {
+    		m_logger.info("MyConfiguration"); // this gets called at application startup, not session startup so this is an app bean.
+    	}
+// Must not instantiate this here. MyUI is a session bean, but it is already instantiated before we get here
+//    	@Bean
+//    	public MyUI getMyUI() {
+//    		return new MyUI();
+//    	}
+
+    	// needed for @PropertySource
+    	@Bean
+    	public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+    		return new PropertySourcesPlaceholderConfigurer();
+    	}
+    	@Bean(name="hints")
+    	@UIScope
+    	public Hints getHints() {
+    		return new HintsImpl();
+    	}
+    }
     @Override
     protected void init(VaadinRequest vaadinRequest) {
         final VerticalLayout layout = new VerticalLayout();
@@ -37,8 +109,8 @@ public class MyUI extends UI {
 
     }
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+//    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+//    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+//    public static class MyUIServlet extends VaadinServlet {
+//    }
 }
